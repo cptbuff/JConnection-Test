987e27a7b6cd92a311ab30ec07f756cef58273e4
cptbuff <francisjkavanagh@gmail.com>
2025-04-17 20:53:20 +0100

    Added code to connect to DB

--- src/jconnection/JConnection.java
+++ src/jconnection/JConnection.java
@@ -12,11 +12,32 @@ import java.sql.Statement;
 
 public class JConnection {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String url = "jdbc:mysql://localhost:3306/user"; // Database URL
+        String user = "root"; // Default XAMPP username
+        String password = ""; // Default XAMPP password is empty
+
+        try {
+            // Load MySQL JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try ( // Establish Connection
+                    Connection conn = DriverManager.getConnection(url, user, password)) {
+                System.out.println("Connected to MySQL!");
+
+                try ( // Create and Execute SQL Query
+                        Statement stmt = conn.createStatement(); ResultSet rs
+                        = stmt.executeQuery("SELECT * FROM user1")) {
+
+                    // Print Data
+                    while (rs.next()) {
+                        System.out.println("ID: " + rs.getInt("ID")
+                                + ", Name: " + rs.getString("Name")
+                                + ", Email: " + rs.getString("Email"));
+                    }
+                    // Close Connection
+                }
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+        }
     }
-    
 }
